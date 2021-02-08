d09f519d7b8f35d1b87577bcdbc4c7f83e979c44
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2021-02-08 15:20:56 -0600

    3

--- efs/efs/settings.py
+++ efs/efs/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio'
+    'portfolio',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
--- efs/portfolio/admin.py
+++ efs/portfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import Customer, Investment, Stock
 
 
 class CustomerList(admin.ModelAdmin):
@@ -10,5 +10,19 @@ class CustomerList(admin.ModelAdmin):
     ordering = ['cust_number']
 
 
+class InvestmentList(admin.ModelAdmin):
+    list_display = ('customer', 'category', 'description', 'recent_value')
+    list_filter = ('customer', 'category')
+    search_fields = ('customer', 'category')
+    ordering = ['customer']
 
-admin.site.register(Customer)
+class StockList(admin.ModelAdmin):
+    list_display = ('customer','symbol', 'name', 'shares', 'purchase_price')
+    list_filter = ('customer','symbol', 'name')
+    search_fields = ('customer','symbol', 'name')
+    ordering = ['customer']
+
+
+admin.site.register(Customer, CustomerList)
+admin.site.register(Investment, InvestmentList)
+admin.site.register(Stock, StockList)
