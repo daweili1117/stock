074635c22a32007de51d0825c05622cf41db7cec
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2021-02-08 15:45:15 -0600

    2

--- efs/efs/urls.py
+++ efs/efs/urls.py
@@ -20,5 +20,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 
 ]
